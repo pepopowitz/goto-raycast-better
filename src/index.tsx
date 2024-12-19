27c656a9ab91a2eec09e550aa8df755ffbc09ce8
Jon Allured <jon@jonallured.com>
2024-12-18 21:50:07 -0600

    Use recursion to render route levels
    Co-authored-by: Steven Hicks <steven.j.hicks@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
+import { Route, routes } from "./routes";
 import { ActionPanel, List, Action } from "@raycast/api";
 
 export default function Command() {
+  return <ChildActions routes={routes} />;
+}
+
+interface ChildActionsProps {
+  routes: Array<Route>;
+}
+
+function ChildActions({ routes }: ChildActionsProps) {
   return (
     <List>
       {routes.map((route) => {
@@ -20,30 +29,6 @@ export default function Command() {
         }
 
         return <List.Item icon="list-icon.png" title={route.name} key={route.name} actions={childAction} />;
-      })}
-    </List>
-  );
-}
-
-interface ChildActionsProps {
-  routes: Array<Route>;
-}
-
-function ChildActions({ routes }: ChildActionsProps) {
-  return (
-    <List>
-      {routes.map((route) => {
-        return (
-          <List.Item
-            icon="list-icon.png"
-            title={route.name}
-            actions={
-              <ActionPanel>
-                <Action.OpenInBrowser url={route.url} title={route.name} />
-              </ActionPanel>
-            }
-          />
-        );
       })}
     </List>
   );
