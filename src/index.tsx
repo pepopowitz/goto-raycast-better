89e3514d5a2205150dfb79ed4039f352f972420b
Jon Allured <jon@jonallured.com>
2024-12-18 21:41:04 -0600

    Clean up TS
    Co-authored-by: Steven Hicks <steven.j.hicks@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,30 @@
-import { ActionPanel, List, Action, Detail } from "@raycast/api";
+import { ActionPanel, List, Action } from "@raycast/api";
 
-const routes = [
+const routes: Array<Route> = [
   {
-    code: "gh",
+    name: "gh",
     url: "https://github.com",
     routes: [
       {
         name: "docs",
         url: "https://github.com/camunda/camunda-docs",
+        routes: [],
       },
       {
         name: "me",
         url: "https://github.com/pepopowitz",
+        routes: [],
       },
     ],
   },
   {
-    code: "google",
+    name: "google",
     url: "https://google.com",
     routes: [],
   },
 ];
 
-export default function Command(props) {
-  console.log(props.arguments);
+export default function Command() {
   return (
     <List>
       {routes.map((route) => {
@@ -31,24 +32,34 @@ export default function Command(props) {
         if (route.routes.length === 0) {
           childAction = (
             <ActionPanel>
-              <Action.OpenInBrowser url={route.url} title={route.code} />
+              <Action.OpenInBrowser url={route.url} title={route.name} />
             </ActionPanel>
           );
         } else {
           childAction = (
             <ActionPanel>
-              <Action.Push title={route.code} target={<ChildActions routes={route.routes} />} />
+              <Action.Push title={route.name} target={<ChildActions routes={route.routes} />} />
             </ActionPanel>
           );
         }
 
-        return <List.Item icon="list-icon.png" title={route.code} key={route.code} actions={childAction} />;
+        return <List.Item icon="list-icon.png" title={route.name} key={route.name} actions={childAction} />;
       })}
     </List>
   );
 }
 
-function ChildActions({ routes }) {
+interface Route {
+  name: string;
+  url: string;
+  routes: Array<Route>;
+}
+
+interface ChildActionsProps {
+  routes: Array<Route>;
+}
+
+function ChildActions({ routes }: ChildActionsProps) {
   return (
     <List>
       {routes.map((route) => {
