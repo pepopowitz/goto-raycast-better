527f6880a464c09ec0a0d6e7b1635b96e3c266e6
Steven Hicks <steven.j.hicks@gmail.com>
2025-02-12 23:19:41 -0600

    cache routes from configuration file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
-import { routes } from "./routes";
+// import { routes } from "./routes";
+import { useEffect, useState } from "react";
 import { RouteList } from "./RouteList";
+import { getRoutes, Route } from "./routes";
 
 export default function Command() {
+  const [routes, setRoutes] = useState<Array<Route>>([]);
+
+  useEffect(() => {
+    async function fetchRoutes() {
+      const routes = await getRoutes();
+      setRoutes(routes);
+    }
+    fetchRoutes();
+  }, []);
+
   return <RouteList routes={routes} />;
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,53 +1,29 @@
+import { getPreferenceValues, Cache } from "@raycast/api";
 export interface Route {
   name: string;
   url: string;
   routes: Array<Route>;
 }
 
-export const routes: Array<Route> = [
-  {
-    name: "gh (GitHub)",
-    url: "https://github.com",
-    routes: [
-      {
-        name: "docs",
-        url: "https://github.com/camunda/camunda-docs",
-        routes: [
-          {
-            // gh docs issues
-            // gh docs pull/123
-            name: "*",
-            url: "https://github.com/camunda/camunda-docs/${0}",
-            routes: [],
-          },
-          {
-            name: "pulls",
-            url: "https://github.com/camunda/camunda-docs/pulls",
-            routes: [],
-          },
-        ],
-      },
-      {
-        name: "me",
-        url: "https://github.com/pepopowitz",
-        routes: [],
-      },
-    ],
-  },
-  {
-    name: "google",
-    url: "https://google.com",
-    routes: [
-      {
-        name: "*",
-        url: "https://google.com?q=${0}",
-        routes: [],
-      },
-      {
-        name: "google",
-        url: "https://google.com",
-        routes: [],
-      },
-    ],
-  },
-];
+interface Preferences {
+  goto_path: string;
+}
+
+export async function getRoutes(): Promise<Array<Route>> {
+  const cache = new Cache();
+  const cached = cache.get("routes");
+  if (cached) {
+    return Promise.resolve(JSON.parse("[]"));
+  }
+
+  const preferences = getPreferenceValues<Preferences>();
+  // TODO: Fetch routes from preferences.goto_path
+  const routes: Array<Route> = [];
+  cache.set("routes", JSON.stringify(routes));
+  return Promise.resolve(routes);
+}
+
+export async function clearRoutes() {
+  const cache = new Cache();
+  cache.clear();
+}
